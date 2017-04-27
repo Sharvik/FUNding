b4a3585de5f753ec2a54d3fd257777cc113e5979
usuario <usuario@10.22.147.35>
2017-04-27 13:04:34 +0100

    FUNding v1.0

---
+++ src/funding/Usuario.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package funding;
+
+/**
+ *
+ * @author usuario
+ */
+public class Usuario {
+    private String dni;
+    private String nombre;
+    private int edad;
+    private String email;
+    private String nacionalidad;
+    private String contraseña;
+    private String;
+
+    public Usuario(String name) {     
+    }
+}
